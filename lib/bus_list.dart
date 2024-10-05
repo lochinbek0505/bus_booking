371a30a27210b8a965f3a7b5a54990156eb54a1e
yengilmas <lbek458@gmail.com>
2024-10-05 18:53:15 +0500

    Bus list finish

--- lib/bus_list.dart
+++ lib/bus_list.dart
@@ -1,42 +1,233 @@
 import 'package:flutter/material.dart';
 
-import 'bus_seat_page.dart';
-
 class BusListScreen extends StatelessWidget {
   final String direction;
-  final DateTime date;
+  final String date;
 
   BusListScreen({required this.direction, required this.date});
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(title: Text('$direction')),
-      body: ListView(
-        children: [
-          ListTile(
-            title: Text('Bus 1'),
-            subtitle: Text('Price: IDR 10,000'),
-            onTap: () {
-              // Navigate to seat selection screen
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => SeatSelectionScreen()));
-            },
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            '$direction',
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
-          ListTile(
-            title: Text('Bus 2'),
-            subtitle: Text('Price: IDR 15,000'),
-            onTap: () {
-              // Navigate to seat selection screen
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => SeatSelectionScreen()));
-            },
+        ),
+        backgroundColor: Colors.orange,
+      ),
+      body: ListView.builder(
+        itemCount: 3,
+        itemBuilder: (BuildContext context, int index) {
+          return ticket_card(size);
+        },
+      ),
+    );
+  }
+
+  Widget ticket_card(Size size) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Card(
+        color: Colors.white,
+        child: Container(
+          width: size.width / 1.2,
+          height: 190,
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 12.0, horizontal: 15),
+                child: Row(
+                  children: [
+                    Card(
+                      color: Colors.orange,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(7),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Container(
+                          height: 25,
+                          child: Center(
+                            child: Text(
+                              "Fastest",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 7,
+                    ),
+                    Card(
+                      color: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(7),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Container(
+                          height: 25,
+                          child: Center(
+                            child: Text(
+                              "Men Only",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Row(
+                  children: [
+                    Row(
+                      children: [
+                        Image.asset(
+                          "assets/location_green.png",
+                          width: 30,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "From",
+                              style: TextStyle(
+                                fontSize: 12,
+                              ),
+                            ),
+                            Text(
+                              "K . Bali",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 13,
+                                  color: Colors.black),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Card(
+                          color: Colors.grey[100],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(7),
+                          ),
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: Container(
+                              height: 23,
+                              child: Center(
+                                child: Text(
+                                  "Est. 30 min",
+                                  style: TextStyle(fontSize: 13),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 7,
+                        ),
+                        Image.asset(
+                          "assets/circle.png",
+                          width: 30,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "To",
+                              style: TextStyle(
+                                fontSize: 12,
+                              ),
+                            ),
+                            Text(
+                              "K . Bali",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 13,
+                                  color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(
+                              "assets/bus.png",
+                              height: 30,
+                            ),
+                            Text(
+                              "Bus 21",
+                              style: TextStyle(
+                                  fontSize: 17, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10.0, vertical: 3),
+                          child: Text(
+                            "Arrival in 04:00 PM",
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 3),
+                      child: Text(
+                        "12 \$ / per",
+                        style: TextStyle(
+                            color: Colors.orange,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
